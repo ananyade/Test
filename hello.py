20fc3d65b67ece3c2566f177547313e94bf6f731
ayanendude <ayanendu_de@yahoo.co.uk>
2019-08-13 20:27:31 -0400

    commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Hello Ananya")
+char_name = "Ananya De"
+char_age = 30
+print (len(char_name))
+print (char_name[3])
+print (char_name.index("a"))
+print (char_name.replace("Ananya", "Ayanendu"))
+print ("The character name is " + char_name.lower() + "..... ")
+print (char_name + "'s age is " + str(char_age) + " years")
+print(pow(4, 2))
+print (1.234524524 + 3.453464343)
